3b043c10c51cb27e04d9178580b986015deeea7a
woyelaishishiba <33919477+woyelaishishiba@users.noreply.github.com>
2019-06-05 18:17:33 +0800

    Update myconfig.js
    改变真正的key，不影响代码功能，但是权限验证会出问题

--- pages/config/myconfig.js
+++ pages/config/myconfig.js
@@ -1,3 +1,3 @@
 export const myUrl = "http://152.136.130.53:8080";
 // export const myUrl = "http://localhost:8080";
-export const myKey = "0123456789";
+export const myKey = "for safe using, I changed this, this is not real key";
